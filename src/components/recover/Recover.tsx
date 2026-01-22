f36f3cb543d8f5b24c4dd74a881b5010e5f78d10
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-23 00:00:08 +0400

    add Recover component

---
+++ src/components/recover/Recover.tsx
@@ -0,0 +1,38 @@
+const Recover: React.FC = () => {
+  return (
+    <>
+      {/* this first line is temporrary and should be deleted while routing for correct design*/}
+      <div className="w-full h-screen px-4 lg:flex lg:justify-center">
+        <div className="w-full h-108 bg-white rounded-xl p-8 flex flex-col items-center lg:w-md">
+          <div className="w-16 h-16 bg-light-green rounded-4xl flex justify-center items-center mb-4">
+            <img className="w-8 h-8" src="/lock.svg" alt="lock icon" />
+          </div>
+
+          <h1 className="w-auto text-3xl font-normal mb-2">Recover Password</h1>
+          <h3 className="text-[13px] font-normal text-light-grey mb-8">
+            Enter your email to receive reset instructions
+          </h3>
+
+          <div className="w-full h-19.5 mb-6">
+            <h4 className="text-[14px] font-normal text-gray mb-2">Email</h4>
+            <input
+              className="w-full h-12 border border-[#D1D5DB] text-black text-[16px] rounded-lg px-4 py-3 placeholder:opacity-50"
+              type="email"
+              placeholder="example@email.com"
+            />
+          </div>
+
+          <button className="w-full h-12 bg-green rounded-lg text-white text-[15px] font-normal mb-6">
+            Send recover link
+          </button>
+
+          <button className="text-[13px] font-normal text-green">
+            Back to log in
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Recover;
