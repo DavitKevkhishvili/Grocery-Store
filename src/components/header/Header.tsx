0957f0a6a26828912c1692cc26d76805b31cba7d
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-22 16:57:02 +0400

    add cursor style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,19 +4,19 @@ const Header: React.FC = () => {
       <div className="w-full h-16 bg-white flex justify-between items-center p-4 mb-12">
         <div className="flex gap-2">
           <img className="w-8 h-8" src="/leaf.svg" alt="leaf icon" />
-          <h1 className="text-3xl h-8 -mt-0.75">Bostani</h1>
+          <h1 className="text-3xl h-8 -mt-0.75">Fresh Market</h1>
         </div>
 
         <div className="flex gap-4">
-          <button>
+          <button className="cursor-pointer">
             <img src="/cart.svg" alt="cart icon" />
           </button>
 
-          <button>
+          <button className="cursor-pointer">
             <img className="w-6 h-6" src="/human.svg" alt="human icon" />
           </button>
 
-          <button>
+          <button className="cursor-pointer">
             <img src="/menu.svg" alt="menu icon" />
           </button>
         </div>
