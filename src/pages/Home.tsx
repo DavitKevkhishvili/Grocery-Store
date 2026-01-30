992ce4e7411c1f29d5aa1bb9a50f8daa20f08cb4
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-30 15:52:01 +0400

    change layout styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
+        <div className="w-full h-24 bg-white rounded-lg"></div>
         <Product />
         <Product />
         <Product />
