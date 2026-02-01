b862cbe39a78f9374de657931820d54346557517
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-01 17:25:48 +0400

    take out code as a Filter component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
+import Filter from "../components/filter/Filter";
 import Product from "../components/product/Product";
 
 const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
-        <div className="w-full h-24 bg-white rounded-lg [box-shadow:0_4px_20px_rgba(0,0,0,0.1)]"></div>
+        <Filter />
         <Product />
         <Product />
         <Product />
