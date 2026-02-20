be4bf3531d601ef2ada88a7e2761e62d45ac3504
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-20 18:13:34 +0400

    improve layout

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -3,10 +3,10 @@ import CartProduct from "../cartProduct/CartProduct";
 const Cart: React.FC = () => {
   return (
     <>
-      <div className="w-70 absolute top-17 right-14 lg:top-30 lg:right-30">
+      <div className="w-70 absolute top-17 right-14 lg:top-30 lg:right-30 [box-shadow:0_4px_20px_rgba(0,0,0,0.1)] rounded-lg">
         <div
           onClick={(e) => e.stopPropagation()}
-          className="w-70 min-h-24 max-h-72 bg-white rounded-t-lg  [box-shadow:0_4px_20px_rgba(0,0,0,0.1)] flex flex-col  items-center divide-y divide-[#D1D5DB] overflow-hidden overflow-y-scroll [&::-webkit-scrollbar]:hidden border-b border-[#D1D5DB]"
+          className="w-70 min-h-30 max-h-89.25 bg-white rounded-t-lg   flex flex-col  items-center divide-y divide-[#D1D5DB] overflow-hidden overflow-y-scroll [&::-webkit-scrollbar]:hidden border-b border-[#D1D5DB]"
         >
           {/* <h3 className="text-[13px] text-light-grey/70 text-center">
           თქვენი კალათა ცარიელია!
@@ -20,7 +20,32 @@ const Cart: React.FC = () => {
           <CartProduct />
           <CartProduct />
         </div>
-        <div className="w-full h-12 bg-white rounded-b-lg"></div>
+        <div className="w-full h-18 flex justify-between p-3 items-center bg-white rounded-b-lg">
+          <div className="flex items-center text-[16px] text-green font-normal">
+            <span className="text-dark">ჯამი:</span>
+            <span>18</span>
+            <svg
+              className="-ml-0.5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 13a6 6 0 1 0 -6 6" />
+              <path d="M6 19h12" />
+              <path d="M10 5v7" />
+              <path d="M14 12v-7" />
+            </svg>
+          </div>
+          <button className="w-22 h-10 flex justify-center items-center rounded-lg bg-green text-white text-[13px] hover:[box-shadow:0_0_15px_rgba(22,163,74,0.3)] cursor-pointer">
+            შეკვეთა
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/cartProduct/CartProduct.tsx
+++ src/components/cartProduct/CartProduct.tsx
@@ -1,35 +1,52 @@
+// import { useState } from "react";
+
 const CartProduct: React.FC = () => {
+  // const [quantity, setQuantity] = useState(0);
   return (
     <>
-      <div className="w-full h-full p-3 flex">
+      <div className="w-full h-30 p-3 flex items-center cursor-pointer hover:bg-light-grey/2">
         <img
           className="w-18 h-18 rounded-lg mr-5"
           src="apples.jpg"
           alt="product image"
         />
         <div className="w-full  flex flex-col justify-between">
-          <h4 className="text-[16px] text-dark font-normal">ვაშლი</h4>
-          <div className="text-[12px] text-grey font-normal">
-            <div className="flex gap-1">
-              <div className="flex items-center">
-                <h5>4.50</h5>
-                <img
-                  className="w-2.5 h-2.5 -mt-0.5"
-                  src="lari.svg"
-                  alt="lari icon"
-                />
-              </div>
-              <span className="font-bold">X</span>
-              <h5>4</h5>
-            </div>
+          <h4 className="text-[16px] text-dark font-normal mb-3">ვაშლი</h4>
 
-            <div className="flex items-center text-[13px] text-dark font-bold">
-              სულ: 18
-              <img
-                className="w-2.5 h-2.5 -mt-0.5"
-                src="lari.svg"
-                alt="lari icon"
-              />
+          <div className="w-24 h-7.5 text-light-grey text-[13px] mb-1">
+            <button className="w-6 h-full border border-light-grey/30 rounded-l-lg hover:bg-green hover:text-white cursor-pointer">
+              -
+            </button>
+            <button className="w-6 h-full border border-light-grey/30 hover:bg-green hover:text-white cursor-pointer">
+              1
+            </button>
+            <button className="w-6 h-full border border-light-grey/30 rounded-r-lg hover:bg-green hover:text-white cursor-pointer">
+              +
+            </button>
+          </div>
+
+          <div className="text-[16px] text-grey font-normal flex items-center gap-1">
+            <h5>1</h5>
+            <span className="">x</span>
+            <div className="flex text-green">
+              <h5>4.50</h5>
+              <svg
+                className="mt-0.5 -ml-0.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="19"
+                height="19"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 13a6 6 0 1 0 -6 6" />
+                <path d="M6 19h12" />
+                <path d="M10 5v7" />
+                <path d="M14 12v-7" />
+              </svg>
             </div>
           </div>
         </div>
@@ -46,35 +63,35 @@ const CartProduct: React.FC = () => {
             <path
               d="M8.33334 9.1665V14.1665"
               stroke="currentColor"
-              strokeWidth="2"
+              strokeWidth="1"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M11.6667 9.1665V14.1665"
               stroke="currentColor"
-              strokeWidth="2"
+              strokeWidth="1"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M15.8333 5V16.6667C15.8333 17.1087 15.6577 17.5326 15.3452 17.8452C15.0326 18.1577 14.6087 18.3333 14.1667 18.3333H5.83332C5.3913 18.3333 4.96737 18.1577 4.65481 17.8452C4.34225 17.5326 4.16666 17.1087 4.16666 16.6667V5"
               stroke="currentColor"
-              strokeWidth="2"
+              strokeWidth="1"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M2.5 5H17.5"
               stroke="currentColor"
-              strokeWidth="2"
+              strokeWidth="1"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M6.66666 4.99984V3.33317C6.66666 2.89114 6.84225 2.46722 7.15481 2.15466C7.46737 1.8421 7.8913 1.6665 8.33332 1.6665H11.6667C12.1087 1.6665 12.5326 1.8421 12.8452 2.15466C13.1577 2.46722 13.3333 2.89114 13.3333 3.33317V4.99984"
               stroke="currentColor"
-              strokeWidth="2"
+              strokeWidth="1"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
