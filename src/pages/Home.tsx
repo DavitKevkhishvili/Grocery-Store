f1da9c4f63b08046e1d4b5205601aeb3f6b0bf13
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-23 16:31:58 +0400

    add error for empty page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
 import SearchBar from "../components/searchBar/SearchBar";
 import Product from "../components/product/Product";
-import { products } from "../data";
+import { useContext } from "react";
+import type { MainContextType } from "../types";
+import { MainContext } from "../contexts/MainContext";
 
 const Home: React.FC = () => {
+  const { filteredProducts } = useContext<MainContextType>(MainContext);
+
   return (
     <>
       <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
         <SearchBar />
 
-        {products.map((p) => (
-          <Product key={p.id} product={p} />
-        ))}
+        {filteredProducts.length === 0 ? (
+          <h1 className="text-xl text-light-grey">პროდუქტი არ მოიძებნა!</h1>
+        ) : (
+          filteredProducts.map((p) => <Product key={p.id} product={p} />)
+        )}
       </div>
     </>
   );
